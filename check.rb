74a2bb7a32664b244d723afab8524d1653d4adb6
Ed Jefferson <e.jefferson@gmail.com>
2016-04-16 08:38:33 +0100

    some stuff

---
+++ check.rb
@@ -0,0 +1,15 @@
+
+
+
+streamclient = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+  config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+end
+
+
+topics = ["#shippamspaste"]
+streamclient.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
