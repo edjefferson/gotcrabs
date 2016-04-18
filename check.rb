edb299143c219092fdd7e24175c8783b17176a04
Ed Jefferson <e.jefferson@gmail.com>
2016-04-18 17:09:12 +0100

    test working

--- check.rb
+++ check.rb
@@ -1,31 +1,72 @@
-
-
-
+require 'twitter'
+require 'tempfile'
+require 'RMagick'
+include Magick
 streamclient = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
   config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
   config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
   config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
 end
+twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+  config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+end
 
-
-topics = ["#shippamspaste"]
+topics = ["#testhash141"]
 streamclient.filter(track: topics.join(",")) do |object|
-  puts object.text if object.is_a?(Twitter::Tweet)
+  if object.is_a?(Twitter::Tweet)
+    name = object.user.name.split(" ")[0]
+    screen_name = object.user.screen_name
+    tweet_id = object.id
+  
+    words =""
+    if name.length > 12
+      words = "You've\ngot\ncrabs!"
+    else
+    words = "#{name},\nyou've\ngot\ncrabs!"
+  end 
+  puts words
+    sleep 5
+    
+    text = Magick::Draw.new
+    text.font_family = 'helvetica'
+    text.pointsize = 52
+    text.gravity = Magick::CenterGravity
+
+    image = ImageList.new("crabs.jpg")
+
+    colours = ["red","blue","green"]
+
+       text.annotate(image, 0,0,-2,-2, words) {
+            self.fill = 'white'
+         }
+         
+         text.annotate(image, 0,0,2,2, words) {
+              self.fill = 'white'
+           }
+
+           text.annotate(image, 0,0,0,0, words) {
+                self.fill = colours.sample
+             }
+
+    temp = Tempfile.new("image")
+    image.write("jpg:"+ temp.path)
+
+    media_ids = %w(test.mp4).map do |filename|
+      Thread.new do
+        twitter_client.upload(File.new temp.path)
+      end
+    end.map(&:value)
+
+    twitter_client.update("@#{screen_name}", :media_ids => media_ids.join(','), :in_reply_to_status_id => tweet_id)
+end
 end
 
 
-require 'RMagick'
-
-include Magick
 
 
 
 
-
-	image = ImageList.new("crabs.jpg")
-
-	cat.rotate!(angle)
-	cat.trim!
-
-	cat.write("pics/#{angle}.jpg")
--- upload.rb
+++ upload.rb
@@ -1,4 +1,5 @@
 require 'twitter'
+require 'tempfile'
 puts ENV["YOUR_CONSUMER_KEY"]
 twitter_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
@@ -16,14 +17,25 @@ include Magick
 
 
 
+words = "chips \n in \n brown \n gravy"
 
+text = Magick::Draw.new
+text.font_family = 'helvetica'
+text.pointsize = 52
+text.gravity = Magick::CenterGravity
 
-image = Image.new("test.png")
+image = ImageList.new("test.png")
 
+ text.annotate(image, 0,0,0,0, words) {
+      self.fill = 'darkred'
+   }
 
-media_ids = %w(test.png).map do |filename|
+temp = Tempfile.new("image")
+image.write("png:"+ temp.path)
+
+media_ids = %w(test.mp4).map do |filename|
   Thread.new do
-    twitter_client.upload(File.new filename)
+    twitter_client.upload(File.new temp.path)
   end
 end.map(&:value)
 
